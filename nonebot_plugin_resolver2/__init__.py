419baafbd4d528d71ffe05ff90c5e62ee1e9de67
Les Freire <64878354+fllesser@users.noreply.github.com>
2024-12-04 22:09:39 +0800

    chore: 拼写错误

--- nonebot_plugin_resolver2/__init__.py
+++ nonebot_plugin_resolver2/__init__.py
@@ -23,11 +23,11 @@ async def _():
     if rconfig.r_ytb_ck:
         save_cookies_to_netscape(rconfig.r_ytb_ck, YTB_COOKIES_FILE, 'youtube.com')
     if rconfig.r_douyin_ck:
-        if douyin := resovlers.pop("douyin", None):
+        if douyin := resolvers.pop("douyin", None):
             douyin.destroy()
             logger.info("未配置抖音 cookie, 故抖音解析器已销毁")
     if rconfig.r_xhs_ck:
-        if xiaohongshu := resovlers.pop("xiaohongshu", None):
+        if xiaohongshu := resolvers.pop("xiaohongshu", None):
             douyin.destroy()
             logger.info("未配置小红书 cookie, 故小红书解析器已销毁")
     # 处理黑名单 resovler
