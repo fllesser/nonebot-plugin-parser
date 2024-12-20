5befcc966847cbc7d1f286a224bc075e32dd205f
Les Freire <64878354+fllesser@users.noreply.github.com>
2024-12-20 14:57:09 +0800

    chore: 更换获取yt-dlp获取版本的方法

--- nonebot_plugin_resolver2/data_source/ytdlp.py
+++ nonebot_plugin_resolver2/data_source/ytdlp.py
@@ -43,7 +43,14 @@ async def update_yt_dlp() -> str:
                 del sys.modules['yt_dlp']
             import yt_dlp
             importlib.reload(yt_dlp)
-            version = pkg_resources.get_distribution('yt-dlp').version
+            # version = pkg_resources.get_distribution('yt-dlp').version
+            version_process = await asyncio.create_subprocess_exec(
+                'yt-dlp', '--version',
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            version_stdout, version_stderr = await version_process.communicate()
+            version = version_stdout.decode().strip()
             success_info = f"Successfully updated yt-dlp, current version: {version}"
             logger.info(success_info)
             return success_info
