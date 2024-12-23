b992cf6a8bd87e5d45219f01ecc1b3050872ff2f
Les Freire <64878354+fllesser@users.noreply.github.com>
2024-12-23 12:48:29 +0800

    update nonebot_plugin_resolver2/constant.py

--- nonebot_plugin_resolver2/constant.py
+++ nonebot_plugin_resolver2/constant.py
@@ -1,13 +1,8 @@
 """
-通用解析
-"""
-# GENERAL_REQ_LINK = "http://47.99.158.118/video-crack/v2/parse?content={}"
-
-"""
 通用头请求
 """
 COMMON_HEADER = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 '
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87'
                   # 'UBrowser/6.2.4098.3 Safari/537.36'
 }
 
@@ -18,5 +13,3 @@ VIDEO_MAX_MB = 100
 
 # 解析列表文件名
 DISABLE_GROUPS = "disable_group_list.json"
-
-############################################################################
