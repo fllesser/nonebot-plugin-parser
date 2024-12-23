637f36de32bb9e38cf166b98fa7c5817d917e566
Les Freire <64878354+fllesser@users.noreply.github.com>
2024-12-23 13:09:25 +0800

    update nonebot_plugin_resolver2/constant.py

--- nonebot_plugin_resolver2/constant.py
+++ nonebot_plugin_resolver2/constant.py
@@ -2,7 +2,7 @@
 通用头请求
 """
 COMMON_HEADER = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 '
                   'UBrowser/6.2.4098.3 Safari/537.36'
 }
 
