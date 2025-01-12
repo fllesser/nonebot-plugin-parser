2d60087ab9f5563d2fff15cfe995cec82b602cf0
fllesser <fllessive@gmail.com>
2025-01-12 13:06:13 +0800

    chore: ytb_cookies_file

--- nonebot_plugin_resolver2/__init__.py
+++ nonebot_plugin_resolver2/__init__.py
@@ -21,7 +21,7 @@ async def _():
     if not rconfig.r_bili_ck:
         logger.warning('未配置哔哩哔哩 cookie，无法使用哔哩哔哩AI总结，可能无法解析 720p 以上画质视频')
     if rconfig.r_ytb_ck:
-        save_cookies_to_netscape(rconfig.r_ytb_ck, YTB_COOKIES_FILE, 'youtube.com')
+        save_cookies_to_netscape(rconfig.r_ytb_ck, ytb_cookies_file, 'youtube.com')
     if not rconfig.r_xhs_ck:
         if xiaohongshu := resolvers.pop("xiaohongshu", None):
             xiaohongshu.destroy()
