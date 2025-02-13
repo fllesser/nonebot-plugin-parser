c10f691182246c73ad96a7f66097b5e12921c376
fllesser <fllessive@gmail.com>
2025-02-13 16:05:58 +0800

    🐞 fix: 更新酷狗音乐解析器以通过标题获取歌曲信息，简化下载逻辑

--- nonebot_plugin_resolver2/parsers/kugou.py
+++ nonebot_plugin_resolver2/parsers/kugou.py
@@ -11,29 +11,48 @@ class KuGou(BaseParser):
             async with session.get(share_url, ssl=False) as response:
                 response.raise_for_status()
                 html_text = await response.text()
+        # <title>土坡上的狗尾草_卢润泽_高音质在线
+        match = re.search(r"<title>(.*)_高音质在线", html_text)
+        if not match:
+            raise ValueError("无法获取歌曲名")
+
+        title = match.group(1).replace("_", " ")
+        api_url = (
+            f"https://www.hhlqilongzhu.cn/api/dg_kgmusic_lingsheng.php?msg={title}&n=1"
+        )
+        async with aiohttp.ClientSession() as session:
+            async with session.get(api_url, headers=self.default_headers) as response:
+                response.raise_for_status()
+                song_info = await response.json()
+        return VideoInfo(
+            title=song_info.get("title"),
+            cover_url=song_info.get("img"),
+            music_url=song_info.get("url"),
+            author=VideoAuthor(name=song_info.get("singer")),
+        )
         # 从 html 中获取 hash 值
         # {"hash":"C76E9235868169CABE26D02D521ED90A"
-        match = re.search(r'{"hash":"([a-zA-Z0-9]+)"', html_text)
-        if not match:
-            raise ValueError("无法获取歌曲 hash 值")
-        hash_value = match.group(1)
-        download_api_url = (
-            f"http://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo&hash={hash_value}"
-        )
-        # http://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo&hash=c76e9235868169cabe26d02d521ed90a
-        async with aiohttp.ClientSession() as session:
-            async with session.get(
-                download_api_url, headers=self.default_headers
-            ) as response:
-                response.raise_for_status()
-                song_info = await response.json(content_type="text/html")
-        return VideoInfo(
-            cover_url=song_info.get("imgUrl"),
-            title=song_info.get("songName"),
-            music_url=song_info.get("url"),
-            video_url="",
-            author=VideoAuthor(name=song_info.get("author_name")),
-        )
+        # match = re.search(r'{"hash":"([a-zA-Z0-9]+)"', html_text)
+        # if not match:
+        #     raise ValueError("无法获取歌曲 hash 值")
+        # hash_value = match.group(1)
+        # download_api_url = (
+        #     f"http://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo&hash={hash_value}"
+        # )
+        # # http://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo&hash=c76e9235868169cabe26d02d521ed90a
+        # async with aiohttp.ClientSession() as session:
+        #     async with session.get(
+        #         download_api_url, headers=self.default_headers
+        #     ) as response:
+        #         response.raise_for_status()
+        #         song_info = await response.json(content_type="text/html")
+        # return VideoInfo(
+        #     cover_url=song_info.get("imgUrl"),
+        #     title=song_info.get("songName"),
+        #     music_url=song_info.get("url"),
+        #     video_url="",
+        #     author=VideoAuthor(name=song_info.get("author_name")),
+        # )
 
     async def parse_video_id(self, video_id: str) -> VideoInfo:
         return await super().parse_video_id(video_id)
