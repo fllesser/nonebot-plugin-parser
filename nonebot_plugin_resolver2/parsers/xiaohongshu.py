c5d66db824ad6baca026bf57ddedb8cafaa9a128
fllesser <fllessive@gmail.com>
2025-03-07 23:35:49 +0800

    🧪 test: add xiaohongshu test

--- nonebot_plugin_resolver2/parsers/xiaohongshu.py
+++ nonebot_plugin_resolver2/parsers/xiaohongshu.py
@@ -1,40 +1,20 @@
 import re
 import json
 import aiohttp
-
 from nonebot.log import logger
-from nonebot.plugin.on import on_message
-from nonebot.adapters.onebot.v11 import Bot, MessageSegment
-from urllib.parse import parse_qs, urlparse
-
-from .filter import is_not_in_disabled_groups
-from .utils import get_video_seg, construct_nodes
-from .preprocess import r_keywords, ExtractText
+from urllib.parse import urlparse, parse_qs
 
 from ..constant import COMMON_HEADER
-from ..download.common import download_imgs_without_raise
-from ..config import rconfig, NICKNAME
+from ..config import rconfig
 
 # 小红书下载链接
 XHS_REQ_LINK = "https://www.xiaohongshu.com/explore/"
 
-xiaohongshu = on_message(
-    rule=is_not_in_disabled_groups & r_keywords("xiaohongshu.com", "xhslink.com")
-)
 
-
-@xiaohongshu.handle()
-async def _(bot: Bot, text: str = ExtractText()):
-    share_prefix = f"{NICKNAME}解析 | 小红书 - "
-
-    if match := re.search(
-        r"(http:|https:)\/\/(xhslink|(www\.)xiaohongshu).com\/[A-Za-z\d._?%&+\-=\/#@]*",
-        text,
-    ):
-        url = match.group(0)
-    else:
-        logger.info(f"{text} ignored")
-        return
+async def parse_url(url: str) -> tuple[str, list[str], str]:
+    """
+    解析小红书 URL
+    """
     # 请求头
     headers = {
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,"
@@ -56,40 +36,37 @@ async def _(bot: Bot, text: str = ExtractText()):
     params = parse_qs(parsed_url.query)
     # 提取 xsec_source 和 xsec_token
     xsec_source = params.get("xsec_source", [None])[0] or "pc_feed"
+    print(xsec_source)
     xsec_token = params.get("xsec_token", [None])[0]
+    print(xsec_token)
     async with aiohttp.ClientSession() as session:
         async with session.get(
             f"{XHS_REQ_LINK}{xhs_id}?xsec_source={xsec_source}&xsec_token={xsec_token}",
             headers=headers,
         ) as resp:
             html = await resp.text()
+    # print(html)
     pattern = r"window.__INITIAL_STATE__=(.*?)</script>"
-    if match := re.search(pattern, html):
-        json_str = match.group(1)
-    else:
-        await xiaohongshu.finish(f"{share_prefix} cookies 可能已失效")
+    matched = re.search(pattern, html)
+    if not matched:
+        raise Exception("小红书 cookies 可能已失效")
+
+    json_str = matched.group(1)
     json_str = json_str.replace("undefined", "null")
     json_obj = json.loads(json_str)
-    note_data = json_obj["note"]["noteDetailMap"][xhs_id]["note"]
-    type = note_data["type"]
-    note_title = note_data["title"]
-    note_desc = note_data["desc"]
-    title_msg = f"{share_prefix}{note_title}\n{note_desc}"
 
-    if type == "normal":
+    logger.debug(f"{json_obj}")
+    note_data = json_obj["note"]["noteDetailMap"][xhs_id]["note"]
+    # 资源类型 normal 图，video 视频
+    resource_type = note_data["type"]
+    # 标题
+    note_title = note_data["title"]
+    # 描述
+    note_desc = note_data["desc"]
+    title_desc = f"{note_title}\n{note_desc}"
+    if resource_type == "normal":
         image_list = note_data["imageList"]
         urls = [item["urlDefault"] for item in image_list]
-        img_path_list = await download_imgs_without_raise(urls)
-        # 发送图片
-        segs = [title_msg] + [
-            MessageSegment.image(img_path) for img_path in img_path_list
-        ]
-        nodes = construct_nodes(bot.self_id, segs)
-        await xiaohongshu.finish(nodes)
-    elif type == "video":
-        await xiaohongshu.send(title_msg)
-        # 这是一条解析有水印的视频
-        # logger.info(note_data['video'])
+    elif resource_type == "video":
         video_url = note_data["video"]["media"]["stream"]["h264"][0]["masterUrl"]
-        # video_url = f"http://sns-video-bd.xhscdn.com/{note_data['video']['consumer']['originVideoKey']}"
-        await xiaohongshu.finish(await get_video_seg(url=video_url))
+    return title_desc, urls, video_url
---
+++ tests/test_xhs.py
@@ -0,0 +1,20 @@
+async def test_xiaohongshu():
+    """
+    xiaohongshu:
+    - https://xhslink.com/a/zGL52ubtpJ20
+    - https://www.xiaohongshu.com/discovery/item/6469c95c0000000012031f3c?source=webshare&xhsshare=pc_web&xsec_token=ABkMJSd3a0BPMgj5BMkZcggIq1FxU8vYNcNW_-MhfDyq0=&xsec_source=pc_share
+    """
+    urls = [
+        "https://xhslink.com/a/zGL52ubtpJ20",
+        "https://www.xiaohongshu.com/discovery/item/6469c95c0000000012031f3c?source=webshare&xhsshare=pc_web&xsec_token=ABkMJSd3a0BPMgj5BMkZcggIq1FxU8vYNcNW_-MhfDyq0=&xsec_source=pc_share",
+    ]
+    from nonebot_plugin_resolver2.parsers.xiaohongshu import parse_url
+
+    for url in urls:
+        try:
+            title_desc, urls, video_url = await parse_url(url)
+            print(title_desc)
+            print(urls)
+            print(video_url)
+        except Exception:
+            continue
