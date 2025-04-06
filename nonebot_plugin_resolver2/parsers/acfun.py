4def7814d8fb86457eb10211fcb48988d109f54f
Les Freire <64878354+fllesser@users.noreply.github.com>
2025-04-06 21:37:11 +0800

    🐳 chore: remove deprecated functions (#91)

--- nonebot_plugin_resolver2/parsers/acfun.py
+++ nonebot_plugin_resolver2/parsers/acfun.py
@@ -2,16 +2,13 @@ import asyncio
 import json
 from pathlib import Path
 import re
-from typing_extensions import deprecated
 
 import aiofiles
 import aiohttp
 
 from ..config import MAX_SIZE, plugin_cache_dir
-from ..download import download_file_by_stream
 from ..download.utils import safe_unlink
 from ..exception import DownloadException, ParseException
-from .utils import escape_special_chars
 
 ACFUN_HEADERS = {
     "referer": "https://www.acfun.cn/",
@@ -40,7 +37,7 @@ async def parse_acfun_url(url: str) -> tuple[str, str]:
     if not matched:
         raise ParseException("解析 acfun 视频信息失败")
     json_str = str(matched.group(1))
-    json_str = escape_special_chars(json_str)
+    json_str = json_str.replace('\\\\"', '\\"').replace('\\"', '"')
     video_info = json.loads(json_str)
 
     video_desc = (
@@ -81,12 +78,10 @@ async def download_acfun_video(m3u8s_url: str, acid: int) -> Path:
         async with aiofiles.open(video_file, "wb") as f, aiohttp.ClientSession() as session:
             total_size = 0
             with tqdm(
-                total=None,
                 unit="B",
                 unit_scale=True,
                 unit_divisor=1024,
                 dynamic_ncols=True,
-                colour="green",
                 desc=video_file.name,
             ) as bar:
                 for url in m3u8_full_urls:
@@ -133,56 +128,3 @@ async def _parse_m3u8(m3u8_url: str):
     m3u8_full_urls = [f"{m3u8_prefix}/{d}" for d in m3u8_relative_links]
 
     return m3u8_full_urls
-
-
-@deprecated("已废弃，请使用 download_acfun_video 代替")
-async def _merge_acs_to_mp4(acid: int, ts_files: list[Path]) -> Path:
-    """合并ac文件到mp4
-
-    Args:
-        acid (int): acid
-        ts_files (list[Path]): ts文件
-
-    Returns:
-        Path: 合并后的mp4文件
-    """
-    from ..download.utils import exec_ffmpeg_cmd
-
-    concat_str = "\n".join([f"file {d.name}" for d in ts_files])
-
-    txt_file = plugin_cache_dir / f"acfun_{acid}.txt"
-    video_file = plugin_cache_dir / f"acfun_{acid}.mp4"
-    async with aiofiles.open(txt_file, "w") as f:
-        await f.write(concat_str)
-    command = [
-        "ffmpeg",
-        "-y",
-        "-f",
-        "concat",
-        "-safe",
-        "0",
-        "-i",
-        str(txt_file),
-        "-c",
-        "copy",
-        str(video_file),
-    ]
-
-    await exec_ffmpeg_cmd(command)
-    return video_file
-
-
-@deprecated("已废弃，请使用 download_acfun_video 代替")
-async def download_and_merge_acfun_video(m3u8s_url: str, acid: int) -> Path:
-    """下载并合并acfun视频
-
-    Args:
-        m3u8s_url (str): m3u8链接
-        acid (int): acid
-
-    Returns:
-        Path: 合并后的mp4文件
-    """
-    m3u8_full_urls = await _parse_m3u8(m3u8s_url)
-    ts_paths = await asyncio.gather(*[download_file_by_stream(url) for url in m3u8_full_urls])
-    return await _merge_acs_to_mp4(acid, ts_paths)
