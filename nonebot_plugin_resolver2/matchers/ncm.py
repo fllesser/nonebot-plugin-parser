fd5e86c0ee989b41f9104517e2a9d27a37adc239
fllesser <fllessive@gmail.com>
2025-04-19 17:31:54 +0800

    🐞 fix: NCM matcher and GitHub Actions workflow
    - Modified the NCM matcher to improve URL handling by integrating a redirect utility for short links.
    - Updated the error logging prefix for consistency.
    - Added a new test file for NCM functionality to enhance test coverage.
    - Uncommented and executed the ffmpeg installation step in the GitHub Actions workflow, ensuring necessary dependencies are available for tests.
    - Introduced additional test runs for encoding and NCM functionalities in the workflow.

--- nonebot_plugin_resolver2/matchers/ncm.py
+++ nonebot_plugin_resolver2/matchers/ncm.py
@@ -3,6 +3,8 @@ import re
 import aiohttp
 from nonebot import logger, on_message
 
+from nonebot_plugin_resolver2.parsers.utils import get_redirect_url
+
 from ..config import NEED_UPLOAD, NICKNAME
 from ..constant import COMMON_HEADER
 from ..download import download_audio, download_img
@@ -24,41 +26,43 @@ ncm = on_message(rule=is_not_in_disabled_groups & r_keywords("music.163.com", "1
 @ncm.handle()
 @handle_exception(ncm)
 async def _(text: str = ExtractText(), keyword: str = Keyword()):
-    share_prefix = f"{NICKNAME}解析 | 网易云 - "
+    pub_prefix = f"{NICKNAME}解析 | 网易云 - "
     # 解析短链接
     url: str = ""
-    if keyword == "163cn.tv":
-        if match := re.search(r"(http:|https:)\/\/163cn\.tv\/([a-zA-Z0-9]+)", text):
-            url = match.group(0)
-            async with aiohttp.ClientSession() as session:
-                async with session.head(url, allow_redirects=False) as resp:
-                    url = resp.headers.get("Location", "")
+    short_pattern = r"(http:|https:)\/\/163cn\.tv\/([a-zA-Z0-9]+)"
+    if keyword == "163cn.tv" and (matched := re.search(short_pattern, text)):
+        url = matched.group(0)
+        url = await get_redirect_url(url)
     else:
         url = text
-    matched = re.search(r"id=(\d+)", url)
+
+    # 获取网易云歌曲id
+    matched = re.search(r"\?id=(\d+)", url)
     if not matched:
-        logger.info(f"{share_prefix}无效链接，忽略 - {text}")
+        logger.info(f"{pub_prefix}无效链接，忽略 - {text}")
         return
     ncm_id = matched.group(1)
 
     # 对接临时接口
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"{NETEASE_TEMP_API.replace('{}', ncm_id)}", headers=COMMON_HEADER) as resp:
+            async with session.get(
+                f"https://www.hhlqilongzhu.cn/api/dg_wyymusic.php?id={ncm_id}&br=7&type=json", headers=COMMON_HEADER
+            ) as resp:
                 resp.raise_for_status()
                 ncm_vip_data = await resp.json()
         ncm_music_url, ncm_cover, ncm_singer, ncm_title = (
             ncm_vip_data.get(key) for key in ["music_url", "cover", "singer", "title"]
         )
     except Exception as e:
-        await ncm.send(f"{share_prefix}错误: {e}")
+        await ncm.send(f"{pub_prefix}错误: {e}")
         raise
-    await ncm.send(f"{share_prefix}{ncm_title} {ncm_singer}" + get_img_seg(await download_img(ncm_cover)))
+    await ncm.send(f"{pub_prefix}{ncm_title} {ncm_singer}" + get_img_seg(await download_img(ncm_cover)))
     # 下载音频文件后会返回一个下载路径
     audio_path = await download_audio(ncm_music_url)
     # 发送语音
     await ncm.send(get_record_seg(audio_path))
-    # 发送群文件
+    # 上传群文件
     if NEED_UPLOAD:
         file_name = keep_zh_en_num(f"{ncm_title}-{ncm_singer}")
         file_name = f"{file_name}.flac"
