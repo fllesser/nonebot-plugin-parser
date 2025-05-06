17b0a03924ef089764c99bb192875874a1ffc320
fllesser <fllessive@gmail.com>
2025-05-06 14:21:58 +0800

    🔖 release v1.8.8

--- tests/test_x.py
+++ tests/test_x.py
@@ -14,17 +14,22 @@ async def test_x():
         "https://x.com/Fortnite/status/1870484479980052921",  # 单图
         "https://x.com/chitose_yoshino/status/1841416254810378314",  # 多图
     ]
-    for url in urls:
+
+    async def parse_x_url_test(url: str):
         logger.info(f"开始解析 {url}")
         video_url, pic_urls = await parse_x_url(url)
         if video_url:
-            logger.info(f"视频: {video_url}")
+            logger.info(f"[{url}] 发现视频: {video_url}")
             video_path = await download_video(video_url)
-            assert video_path
-            logger.success(f"视频下载成功 {url}")
+            assert video_path.exists()
+            logger.success(f"[{url}] 视频解析并下载成功")
         if pic_urls:
-            logger.info(f"图片: {pic_urls}")
+            logger.info(f"[{url}] 发现图片: {pic_urls}")
             tasks = [download_img(url=pic_url) for pic_url in pic_urls]
             img_paths = await asyncio.gather(*tasks)
-            assert img_paths
-            logger.success(f"图片下载成功 {url}")
+            assert len(img_paths) == len(pic_urls)
+            for img_path in img_paths:
+                assert img_path.exists()
+            logger.success(f"[{url}] 图片解析并下载成功")
+
+    await asyncio.gather(*[parse_x_url_test(url) for url in urls])
