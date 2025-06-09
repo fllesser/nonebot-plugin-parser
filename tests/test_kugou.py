72aedaa26d47a9c85f3124223c0a23cdd9d30e33
fllesser <fllessive@gmail.com>
2025-06-09 12:32:58 +0800

    🗑️ remove Kugou and NCM parsers and matchers; update README and tests accordingly

--- tests/test_kugou.py
+++ tests/test_kugou.py
@@ -5,7 +5,7 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_ncm():
+async def test_kugou():
     from nonebot_plugin_resolver2.download import download_audio
     from nonebot_plugin_resolver2.parsers import KuGouParser
 
@@ -13,7 +13,7 @@ async def test_ncm():
 
     urls = ["https://t3.kugou.com/song.html?id=AeZ3f7EqV2"]
 
-    async def test_parse_ncm(url: str) -> None:
+    async def parse_kugou(url: str) -> None:
         logger.info(f"{url} | 开始解析酷狗音乐")
 
         result = await parser.parse_share_url(url)
@@ -25,4 +25,14 @@ async def test_ncm():
         assert audio_path
         logger.success(f"{url} | 网易云音乐解析成功")
 
-    await asyncio.gather(*[test_parse_ncm(url) for url in urls])
+    await asyncio.gather(*[parse_kugou(url) for url in urls])
+
+
+async def test_kugou_audio_url():
+    from nonebot_plugin_resolver2.parsers import KuGouParser
+
+    parser = KuGouParser()
+
+    audio_url = await parser.get_audio_url("1hfw6baEmV3")
+    assert audio_url
+    logger.success(f"酷狗音乐音频URL: {audio_url}")
