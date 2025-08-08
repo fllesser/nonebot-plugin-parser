5249c98c292aeff5e14101f18d50e9bae3686c3b
fllesser <fllessive@gmail.com>
2025-08-08 19:29:02 +0800

    🐛 fix: 优化微博解析，移除冗余代码

--- src/nonebot_plugin_resolver2/matchers/weibo.py
+++ src/nonebot_plugin_resolver2/matchers/weibo.py
@@ -1,7 +1,4 @@
-import json
-
-from nonebot import logger, on_message
-from nonebot.adapters.onebot.v11 import MessageEvent
+from nonebot import on_message
 
 from ..config import NICKNAME
 from ..download import DOWNLOADER
@@ -9,7 +6,7 @@ from ..exception import handle_exception
 from ..parsers import WeiBoParser
 from .filter import is_not_in_disabled_groups
 from .helper import obhelper
-from .preprocess import r_keywords
+from .preprocess import ExtractText, r_keywords
 
 weibo_parser = WeiBoParser()
 
@@ -21,28 +18,18 @@ weibo = on_message(
 
 @weibo.handle()
 @handle_exception()
-async def _(event: MessageEvent):
-    if len(event.message) == 1 and event.message[0].type == "json":  # 处理QQ小程序消息
-        try:
-            data = json.loads(event.message[0].data["data"])
-            message = data["meta"]["detail_1"]["qqdocurl"]
-        except Exception as e:
-            logger.error(f"处理QQ小程序消息时发生错误: {e}")
-            return
-    else:
-        message = event.message.extract_plain_text().strip()
-    video_info = await weibo_parser.parse_share_url(message)
-
-    ext_headers = {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",  # noqa: E501
-        "referer": "https://weibo.com/",
-    }
+async def _(text: str = ExtractText()):
+    video_info = await weibo_parser.parse_share_url(text)
 
     await weibo.send(f"{NICKNAME}解析 | 微博 - {video_info.title} - {video_info.author}")
+
     if video_info.video_url:
-        video_path = await DOWNLOADER.download_video(video_info.video_url, ext_headers=ext_headers)
+        video_path = await DOWNLOADER.download_video(video_info.video_url, ext_headers=weibo_parser.ext_headers)
         await weibo.finish(obhelper.video_seg(video_path))
+
     if video_info.pic_urls:
-        image_paths = await DOWNLOADER.download_imgs_without_raise(video_info.pic_urls, ext_headers=ext_headers)
+        image_paths = await DOWNLOADER.download_imgs_without_raise(
+            video_info.pic_urls, ext_headers=weibo_parser.ext_headers
+        )
         if image_paths:
             await obhelper.send_segments([obhelper.img_seg(path) for path in image_paths])
