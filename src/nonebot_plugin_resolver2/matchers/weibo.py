2d0e31a161dc9c6a57aec609cf8f36d5a50126b8
merlin <merlin@merlinblog.cn>
2025-08-08 18:53:12 +0800

    添加对微博小程序的支持

--- src/nonebot_plugin_resolver2/matchers/weibo.py
+++ src/nonebot_plugin_resolver2/matchers/weibo.py
@@ -1,6 +1,7 @@
-from nonebot import on_keyword
+import json
+
+from nonebot import logger, on_message
 from nonebot.adapters.onebot.v11 import MessageEvent
-from nonebot.rule import Rule
 
 from ..config import NICKNAME
 from ..download import DOWNLOADER
@@ -8,16 +9,28 @@ from ..exception import handle_exception
 from ..parsers import WeiBoParser
 from .filter import is_not_in_disabled_groups
 from .helper import obhelper
+from .preprocess import r_keywords
 
 weibo_parser = WeiBoParser()
 
-weibo = on_keyword(keywords={"weibo.com", "m.weibo.cn"}, rule=Rule(is_not_in_disabled_groups))
+weibo = on_message(
+    rule=is_not_in_disabled_groups & r_keywords("weibo.com", "m.weibo.cn"),
+    priority=5,
+)
 
 
 @weibo.handle()
 @handle_exception()
 async def _(event: MessageEvent):
-    message = event.message.extract_plain_text().strip()
+    if len(event.message) == 1 and event.message[0].type == "json": # 处理QQ小程序消息
+        try:
+            data = json.loads(event.message[0].data["data"])
+            message = data["meta"]["detail_1"]["qqdocurl"]
+        except Exception as e:
+            logger.error(f"处理QQ小程序消息时发生错误: {e}")
+            return
+    else:
+        message = event.message.extract_plain_text().strip()
     video_info = await weibo_parser.parse_share_url(message)
 
     ext_headers = {
