12efa1b3d764467798e4eea18e024d707255582b
fllesser <fllessive@gmail.com>
2025-09-15 17:37:13 +0800

    🐳 chore: remove unused code

--- src/nonebot_plugin_resolver2/parsers/kuaishou.py
+++ src/nonebot_plugin_resolver2/parsers/kuaishou.py
@@ -55,48 +55,6 @@ class KuaishouParser:
             raise ParseException("window.init_state don't contains videos or pics")
 
         return photo.convert_parse_result()
-        # try:
-        #     json_data = json.loads(json_text)
-        # except json.JSONDecodeError as e:
-        #     raise ParseException("failed to parse INIT_STATE payload") from e
-
-        # photo_data = {}
-        # for json_item in json_data.values():
-        #     if "result" in json_item and "photo" in json_item:
-        #         photo_data = json_item
-        #         break
-
-        # if not photo_data:
-        #     raise ParseException("failed to parse photo info from INIT_STATE")
-
-        # # 判断result状态
-        # if (result_code := photo_data["result"]) != 1:
-        #     raise ParseException(f"获取作品信息失败: {result_code}")
-
-        # data = photo_data["photo"]
-
-        # # 获取视频地址
-        # video_content = None
-        # if "mainMvUrls" in data and len(data["mainMvUrls"]) > 0:
-        #     video_url = data["mainMvUrls"][0]["url"]
-        #     video_content = VideoContent(video_url=video_url)
-
-        # # 获取图集
-        # ext_params_atlas = data.get("ext_params", {}).get("atlas", {})
-        # atlas_cdn_list = ext_params_atlas.get("cdn", [])
-        # atlas_list = ext_params_atlas.get("list", [])
-        # images = []
-        # if len(atlas_cdn_list) > 0 and len(atlas_list) > 0:
-        #     for atlas in atlas_list:
-        #         images.append(f"https://{atlas_cdn_list[0]}/{atlas}")
-
-        # video_info = ParseResult(
-        #     title=data["caption"],
-        #     cover_url=data["coverUrls"][0]["url"],
-        #     author=data["userName"],
-        #     content=video_content or ImageContent(pic_urls=images),
-        # )
-        # return video_info
 
 
 from typing import TypeAlias
