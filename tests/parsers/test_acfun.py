d9d1f4a7fd89af5899185995c938f8cdf01cd151
uy/sun <hmy0119@gmail.com>
2025-09-30 21:10:58 +0800

    test: 修复测试

--- tests/parsers/test_acfun.py
+++ tests/parsers/test_acfun.py
@@ -4,6 +4,7 @@ import pytest
 
 @pytest.mark.asyncio
 async def test_parse():
+    from nonebot_plugin_resolver2.download import DOWNLOADER
     from nonebot_plugin_resolver2.download.utils import fmt_size
     from nonebot_plugin_resolver2.parsers import AcfunParser
 
@@ -11,16 +12,18 @@ async def test_parse():
     acfun_parser = AcfunParser()
 
     async def parse_acfun_url(url: str) -> None:
-        acid = int(url.split("/")[-1].split("ac")[1])
-        logger.info(f"{url} | 开始解析视频 acid: {acid}")
-        m3u8s_url, video_desc = await acfun_parser.parse_url(url)
-        assert m3u8s_url
-        assert video_desc
-        logger.debug(f"{url} | m3u8s_url: {m3u8s_url}, video_desc: {video_desc}")
+        logger.info(f"{url} | 开始解析 Acfun 视频")
+        parse_result = await acfun_parser.parse_url(url)
+        logger.debug(f"{url} | 解析结果: \n{parse_result}")
+
+        assert parse_result.title, "视频标题为空"
+        assert parse_result.author, "作者信息为空"
+        assert parse_result.video_url, "视频链接为空"
 
         logger.info(f"{url} | 开始下载视频")
-        video_file = await acfun_parser.download_video(m3u8s_url, acid)
-        assert video_file
-        logger.info(f"{url} | 视频下载成功, 视频{fmt_size(video_file)}")
+        video_path = await DOWNLOADER.download_video(parse_result.video_url, ext_headers=acfun_parser.headers)
+        assert video_path.exists()
+        logger.info(f"{url} | 视频下载成功, 视频{fmt_size(video_path)}")
+        logger.success(f"{url} | Acfun 视频解析成功")
 
     await parse_acfun_url(url)
