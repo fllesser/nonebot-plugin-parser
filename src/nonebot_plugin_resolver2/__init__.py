0724e73ebbaa7d7771320d7b3ef826b2c1901ffe
fllesser <fllessive@gmail.com>
2025-10-14 10:49:16 +0800

    🔖 release(latest): bump version to 1.13.1 and update plugin description

--- src/nonebot_plugin_resolver2/__init__.py
+++ src/nonebot_plugin_resolver2/__init__.py
@@ -12,17 +12,17 @@ from .cookie import save_cookies_to_netscape
 from .matchers import resolvers
 
 __plugin_meta__ = PluginMetadata(
-    name="链接分享自动解析",
-    description="BV号/链接/小程序/卡片 | B站/抖音/快手/微博/小红书/youtube/tiktok/twitter/acfun",
+    name="链接分享解析 ob 版(不再维护)",
+    description="onebot 版, 不再维护, 请使用 nonebot-plugin-parser",
     usage="发送支持平台的(BV号/链接/小程序/卡片)即可",
     type="application",
-    homepage="https://github.com/fllesser/nonebot-plugin-resolver2",
+    homepage="https://github.com/fllesser/nonebot-plugin-parser/tree/resolver2",
     config=Config,
     supported_adapters={"~onebot.v11"},
     extra={
         "author": "fllesser",
         "email": "fllessive@gmail.com",
-        "homepage": "https://github.com/fllesser/nonebot-plugin-resolver2",
+        "homepage": "https://github.com/fllesser/nonebot-plugin-parser/tree/resolver2",
     },
 )
 
